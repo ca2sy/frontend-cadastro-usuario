1653b4a89771fdc007bba8e779bae516d804ee00
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2026-01-12 12:09:46 -0300

    site terminado

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import "./style.css";
+import api from "../../services/api";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+    getUsers();
+  }
+
+      async function deleteUsers(id) {
+      await api.delete(`/usuarios/${id}`);
+      getUsers()
+    }
+
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+        <input type="text" name="nome" placeholder="Nome" ref={inputName} />
+        <input type="number" name="idade" placeholder="Idade" ref={inputAge} />
+        <input type="email" name="idade" placeholder="Email" ref={inputEmail} />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome: <span>{user.name}v</span>
+            </p>
+            <p>
+              Idade: <span> {user.age} </span>
+            </p>
+            <p>
+              Email:
+              <span>{user.email}</span>{" "}
+            </p>
+          </div>
+          <button
+            onClick={() => {
+               deleteUsers(user.id);
+            }}
+          >
+            ❌
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
